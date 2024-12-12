87a671ab295b05814f7a155f6b943c848096bbc8
Polandian <101973563+Polandian@users.noreply.github.com>
2024-12-12 21:08:29 +0300

    Kaynak Kodları

--- h2o.c
+++ h2o.c
@@ -1,23 +1,15 @@
-//Today, we are going to make a simple programming language
-//Written in C
-//In which we MAYBE can update later on if i decide the base code in good enough
-//Because h2o is already a project, i just dont have enough C knowledge to start doing it
-
-//Functions we are going to cover this time:
-/*
--creating own functions (somehow idk how but somehow)
-*/
-
-//By the way im a pretty beginner in C
-//Like i made total of 1 projects in this language and i know absolutely nothing about it
-//So like pls dont call me a dumbass plz ty
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static char varNames[1024][1024];  //variables that user create
-static char varValues[1024][1024]; //value of  them ↑
+char varNames[256][256];  //variables that user create
+char varValues[256][256]; //value of  them ↑
+
+char functionNames[256][256];      //Name of user created functions
+char functionParameters[256][256]; //Parameters of those
+char functionVoid[256][256];       //The code to be ran in functions
+
+int readMode = 0; //0 = compile normally 1 = read function 2 = read if/else statement
 
 #include "include/h2oFunctions.h"
 #include "include/compile.h"
@@ -30,15 +22,26 @@ int main(){
     free(testChar); //frees memory */
 
     //TODO ERROR MESSAGES
+
+    //TODO PASSING VALUES TO THE TEMPORARY FILES CREATED
+    //AND RUN THEM VIA kullan
+    //FUNCTIONS TO SEPERATE VARIABLES AND CREATE FILE+WRITE TO THEM ARE IN THE BEGINNING OF h2oFunctions.h
     
     //TODO FOR USER FUNCTIONS, JUST KEEP READING AND ADD ALL THE LINES UNTIL bitir TO A STRING, AND WHEN CALLED JUST RUN IT FROM THE STRING ARRAY
     //EXAMPLE:
     /*
-    fonksiyon yazma(yazi)
-        yazdir ";yazi;"
-    bitir
+    fonksiyon yazma(yazi){
+        yazdir "&yazi&"
+    }
 
     THIS ↑ IS A FUNCTION
+    
+    IMO, WE CAN CREATE A TEMPORARY FILE
+    RUN IT
+    AND THEN REMOVE IT WITH remove("filename.h2o");
+    yeah this seems smart
+
+    TODO also make some in-built functions to assign a value from a file to a string and etc
     */
 
     system("cls"); //clears screen
@@ -49,7 +52,7 @@ int main(){
     scanf("%1024[^\n]", fs);
 
     system("cls");
-    COMPILE(fs); //compiles the file here
+    COMPILE(fs, 0); //compiles the file here
     free(fs);    //free file name here
 
     char canExit[BUFFER_SIZE];
